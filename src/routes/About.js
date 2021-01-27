92363662cc41b935261deda111fac7f548c76527
tjdgus3160 <tjdgus3160@gmail.com>
2021-01-27 22:01:54 +0900

    완성

---
+++ src/routes/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+    return (
+    <div className="about__container">
+        <span>About this page: I built it because I love movies.</span>
+    </div>
+    )
+}
+
+export default About;
